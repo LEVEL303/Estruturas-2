9aa8fac469810be70a341a3aaaad972c5a39264e
Levitico Rimon <leviticorimon@gmail.com>
2024-07-28 16:15:34 -0300

    Exercício de árvore binária concluído.

--- unidade-1/exercicios/arvore-binaria/arvore.c
+++ unidade-1/exercicios/arvore-binaria/arvore.c
@@ -3,61 +3,115 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct info {
+typedef struct info {
     char nome[100];
-    int mat;
+    int matricula;
     int turma;
     float nota;
-}typedef struct info Info;
+}Info;
 
-struct node {
-    struct info info;
+typedef struct node {
+    Info info;
     struct node * esq;
     struct node * dir;
-}typedef struct node Node;
+}Node;
 
-Node * criarNode(Info info);
-int comparar(Info a, Info b);
-Node inserir(struct node * raiz, struct info info);
+Node * criarNode(Info * info);
+int comparar(Info * a, Info * b);
+Node * inserir(Node * raiz, Info * info);
+void liberarArvore(Node * raiz);
+void preOrdem(Node * raiz);
+void inOrdem(Node * raiz);
+void posOrdem(Node * raiz);
 
 int main() {
-    Node * raiz;
-    Info * info;
+    FILE * arquivo = fopen("alunos.txt", "r");
+    Info info;
+    Node * raiz = NULL;
 
-    info.nome = "Levitico";
-    info.mat = 123;
-    info.turma = 1;
-    info.nota = rand();
-    
+    if (arquivo == NULL) {
+        printf("Falha ao abrir arquivo!\n");
+        exit(1);
+    }
+
+    while(fscanf(arquivo, " %d %d %[^\n]", &info.matricula, &info.turma, info.nome) == 3) {
+        info.nota = (float) (rand() % 10);
+        raiz = inserir(raiz, &info);
+    }
+
+    printf("# Pre-ordem:\n");
+    preOrdem(raiz);
+    printf("# Inordem:\n");
+    inOrdem(raiz);
+    printf("# Pos-ordem:\n");
+    posOrdem(raiz);
+
+    fclose(arquivo);
+    liberarArvore(raiz);
     return 0;
 }
 
-Node * criarNode(Info info) {
+Node * criarNode(Info * info) {
     Node * node = (Node *) malloc(sizeof(Node));
+
+    if (node == NULL) {
+        printf("Falha na alocacao de memoria!\n");
+        exit(1);
+    }
     
-    strcpy(node->info.nome, info.nome);
-    node->info.mat = info.mat;
-    node->info.turma = info.turma;
-    node->info.nota = info.nota;
+    strcpy(node->info.nome, info->nome);
+    node->info.matricula = info->matricula;
+    node->info.turma = info->turma;
+    node->info.nota = info->nota;
     node->esq = NULL;
     node->dir = NULL;
 
     return node;
 }
 
-int comparar(struct info a, struct info b) {
-    return strcmp(a,b);
+int comparar(Info * a, Info * b) {
+    return strcmp(a->nome, b->nome);
 }
 
-Node * inserir(Node * raiz, Info info) {
+Node * inserir(Node * raiz, Info * info) {
     if (raiz == NULL) {
         raiz = criarNode(info);
-    }
-    if (comparar(raiz->info, info) > 0) {
+    } else if (comparar(&raiz->info, info) > 0) {
         raiz->dir = inserir(raiz->dir, info);
-    } else if (comparar(raiz->esq, info) < 0) {
+    } else if (comparar(&raiz->info, info) < 0) {
         raiz->esq = inserir(raiz->esq, info);
     }
-
     return raiz; 
+}
+
+void liberarArvore(Node * raiz) {
+    if (raiz != NULL) {
+        liberarArvore(raiz->esq);
+        liberarArvore(raiz->dir);
+        free(raiz);
+    }
+}
+
+void preOrdem(Node * raiz) {
+    if (raiz != NULL) {
+        printf("%d %d %s %.1f\n", raiz->info.matricula, raiz->info.turma, raiz->info.nome, raiz->info.nota);
+        preOrdem(raiz->esq);
+        preOrdem(raiz->dir);
+    }
+}
+
+void inOrdem(Node * raiz) {
+    if (raiz != NULL) {
+        inOrdem(raiz->esq);
+        printf("%d %d %s %.1f\n", raiz->info.matricula, raiz->info.turma, raiz->info.nome, raiz->info.nota);
+        inOrdem(raiz->dir);
+    }
+}
+
+void posOrdem(Node * raiz) {
+    if (raiz != NULL) {
+        posOrdem(raiz->esq);
+        posOrdem(raiz->dir);
+        printf("%d %d %s %.1f\n", raiz->info.matricula, raiz->info.turma, raiz->info.nome, raiz->info.nota);
+    }
 }
